17eb38dc2af3b94f5c46be199ba8bab9c7b7b827
Tim Conkling <tconkling@gmail.com>
2020-05-19 16:26:01 -0700

    Room generation

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
+import random
+
 import streamlit as st
 
 import SessionState
-from gamelit import GamelitComponent, set_tile, Pos, get_tile
+from gamelit import GamelitComponent, set_tile, Pos, get_tile, TileGrid
 
 # Register Gamelit
 st.register_component("gamelit", GamelitComponent)
 
 # Hello!
 st.title("Gamelit Demo")
-st.text("Controls: arrow keys to move")
+
+st.markdown("""
+An example component that renders tiles and returns keyboard events.
+
+It's a "dumb terminal": the Python script maintains game state,
+runs update logic, and tells the component to render new tiles.
+""")
 
 # Initialize state
 game_state = SessionState.get(
-	initialized=False,
 	player_pos=Pos(1, 1),
+	player_facing="left",
+	room=None,
+	depth=1,
 )
 
 # Tiles
 WALL = "wall.png"
 FLOOR = "floor.png"
+KNIGHT_LEFT = "knight_left.png"
+KNIGHT_RIGHT = "knight_right.png"
 SKULL = "skull1.png"
+CHEST = "chest.png"
+STAIRS = "stairs.png"
 
 # Keys
 LEFT = "ArrowLeft"
@@ -27,49 +41,88 @@ RIGHT = "ArrowRight"
 UP = "ArrowUp"
 DOWN = "ArrowDown"
 
-# Create static background layer
-bg_tiles = [
-	[WALL] * 14,
-	[WALL] + [FLOOR] * 12 + [WALL],
-	[WALL] + [FLOOR] * 12 + [WALL],
-	[WALL] + [FLOOR] * 12 + [WALL],
-	[WALL] + [FLOOR] * 12 + [WALL],
-	[WALL] + [FLOOR] * 12 + [WALL],
-	[WALL] + [FLOOR] * 12 + [WALL],
-	[WALL] + [FLOOR] * 12 + [WALL],
-	[WALL] + [FLOOR] * 12 + [WALL],
-	[WALL] + [FLOOR] * 12 + [WALL],
-	[WALL] + [FLOOR] * 12 + [WALL],
-	[WALL] * 14,
-]
+ROOM_WIDTH = 14
+ROOM_HEIGHT = 12
 
-# Build layers
-layers = []
-if not game_state.initialized:
-	layers.append({"layer": 0, "tiles": bg_tiles})
-	game_state.initialized = True
+def generate_room() -> TileGrid:
+	tiles = []
 
-# Create empty foreground layer
+	# Fill floors
+	for yy in range(0, ROOM_HEIGHT):
+		for xx in range(0, ROOM_WIDTH):
+			set_tile(tiles, FLOOR, Pos(xx, yy))
+
+	# Top/bottom walls
+	for yy in (0, ROOM_HEIGHT - 1):
+		for xx in range(0, ROOM_WIDTH):
+			set_tile(tiles, WALL, Pos(xx, yy))
+
+	# Left/right walls
+	for xx in (0, ROOM_WIDTH - 1):
+		for yy in range(0, ROOM_HEIGHT):
+			set_tile(tiles, WALL, Pos(xx, yy))
+
+	# Pick a spot for the stairs. Ensure it's not where the player is standing.
+	while True:
+		stairs_pos = Pos(
+			random.randint(1, ROOM_WIDTH - 2),
+			random.randint(1, ROOM_HEIGHT - 2),
+		)
+		if stairs_pos != game_state.player_pos:
+			break
+
+	set_tile(tiles, STAIRS, stairs_pos)
+
+	return tiles
+
+tile_layers = []
+
+# Create a room if need be! This will replace any existing room.
+# We only send the room tiles when the room is generated, rather than
+# every tick.
+if game_state.room is None:
+	game_state.room = generate_room()
+	tile_layers.append({"layer": 0, "tiles": game_state.room})
+
+# Create our foreground tiles. We always send these.
 fg_tiles = []
-layers.append({"layer": 1, "tiles": fg_tiles})
+tile_layers.append({"layer": 1, "tiles": fg_tiles})
 
 # Add the player to the foreground
-set_tile(fg_tiles, SKULL, game_state.player_pos)
+player_tile = KNIGHT_LEFT if game_state.player_facing == "left" else KNIGHT_RIGHT
+set_tile(fg_tiles, player_tile, game_state.player_pos)
 
 # Update the game, and get the new keyboard state
-keys = st.gamelit("game", layers)
+keys = st.gamelit("game", tile_layers)
+
+# Show stats
+st.markdown(f"## Dungeon Depth: {game_state.depth * 10} meters")
 
 # Process input. Move the player around with the arrow keys.
 new_pos = game_state.player_pos
+new_facing = game_state.player_facing
 if keys.get(LEFT):
 	new_pos += Pos(-1, 0)
+	new_facing = "left"
 if keys.get(RIGHT):
 	new_pos += Pos(1, 0)
+	new_facing = "right"
 if keys.get(UP):
 	new_pos += Pos(0, -1)
 if keys.get(DOWN):
 	new_pos += Pos(0, 1)
 
 # Detect collisions. Only update the player's position if it's on a floor tile.
-if get_tile(bg_tiles, new_pos) == FLOOR:
+tile = get_tile(game_state.room, new_pos)
+if tile == FLOOR or tile == STAIRS:
 	game_state.player_pos = new_pos
+	game_state.player_facing = new_facing
+
+	# Go down a level!
+	if tile == STAIRS:
+		game_state.room = None
+		game_state.depth += 1
+
+# Show help
+st.text("Controls: arrow keys to move")
+st.text("(Click inside the game to give it keyboard focus)")
