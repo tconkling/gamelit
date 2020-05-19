468923c76545e43c82ae83dab7d564c300bc2fd0
Tim Conkling <tconkling@gmail.com>
2020-05-19 13:59:32 -0700

    Grid drawing

--- app.py
+++ app.py
@@ -1,6 +1,39 @@
 import streamlit as st
 from gamelit import GamelitComponent
 
+WALL = "wall.png"
+FLOOR = "floor.png"
+SKULL = "skull1.png"
+
 st.register_component("gamelit", GamelitComponent)
 
-st.gamelit()
+# Build background layer
+bg_layer = {
+	"layer": 0,
+	"tiles": [
+		[WALL] * 14,
+		[WALL] + [FLOOR] * 12 + [WALL],
+		[WALL] + [FLOOR] * 12 + [WALL],
+		[WALL] + [FLOOR] * 12 + [WALL],
+		[WALL] + [FLOOR] * 12 + [WALL],
+		[WALL] + [FLOOR] * 12 + [WALL],
+		[WALL] + [FLOOR] * 12 + [WALL],
+		[WALL] + [FLOOR] * 12 + [WALL],
+		[WALL] + [FLOOR] * 12 + [WALL],
+		[WALL] + [FLOOR] * 12 + [WALL],
+		[WALL] + [FLOOR] * 12 + [WALL],
+		[WALL] * 14,
+	]
+}
+
+skull_layer = {
+	"layer": 1,
+	"tiles": [
+		None,
+		None,
+		None,
+		[None, None, None, SKULL]
+	]
+}
+
+st.gamelit(key="game", layers=[bg_layer, skull_layer])
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,27 @@
 import * as PIXI from "pixi.js"
-import { Streamlit } from "./streamlit"
+import { RenderData, Streamlit } from "./streamlit"
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./streamlit.css"
 
+// Constants
+const TILE_SIZE = 24
+
+// Globals
+let spritesheet: PIXI.Spritesheet
+let tileLayerRoot = new PIXI.Container()
+let tileLayers = new Map<number, PIXI.Container>()
+
 // Create a PIXI app
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST
-const app = new PIXI.Application({width: window.innerWidth})
-app.stage.scale.set(2, 2)
+const app = new PIXI.Application({width: window.innerWidth, transparent: true})
 document.body.appendChild(app.view)
+
+app.stage.scale.set(2, 2)
+app.stage.addChild(tileLayerRoot)
 
 // Load resources
 PIXI.Loader.shared.add("spritesheet.json").load(onResourcesLoaded)
-
-let spritesheet: PIXI.Spritesheet
 function onResourcesLoaded () {
 	spritesheet = PIXI.Loader.shared.resources["spritesheet.json"].spritesheet!
 
@@ -26,15 +34,59 @@ function onResourcesLoaded () {
 	Streamlit.setFrameHeight()
 }
 
+/** Draw a tile grid. The grid, its rows, and individual cells can all be null. */
+function drawGrid (layerIndex: number, grid: string[][]) {
+	// Create the layer if it doesn't already exist
+	let layer = tileLayers.get(layerIndex)
+	if (layer != null) {
+		layer.removeChildren()
+	} else {
+		layer = new PIXI.Container()
+		layer.zIndex = layerIndex
+		tileLayerRoot.addChild(layer)
+		tileLayers.set(layerIndex, layer)
+	}
+
+	if (grid == null) {
+		return
+	}
+
+	for (let yy = 0; yy < grid.length; ++yy) {
+		const row = grid[yy]
+		if (row == null) {
+			continue
+		}
+
+		for (let xx = 0; xx < row.length; ++xx) {
+			const tileName = row[xx]
+			if (tileName == null) {
+				continue
+			}
+
+			const tileTex = spritesheet.textures[tileName]
+			if (tileTex == null) {
+				console.warn(`Missing tile texture "${tileName}"`)
+				continue
+			}
+
+			const tile = new PIXI.Sprite(tileTex)
+			tile.position.set(xx * TILE_SIZE, yy * TILE_SIZE)
+			layer.addChild(tile)
+		}
+	}
+}
+
 Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, event => {
+	const renderData = (event as CustomEvent<RenderData>).detail
+	const layers = renderData.args["layers"]
+	if (layers == null) {
+		console.warn("Missing 'layers' arg")
+		return
+	}
 
-	const bunny = new PIXI.Sprite(spritesheet.textures["oryx_16bit_fantasy_world_612.png"])
-
-	// Setup the position of the bunny
-	bunny.x = 10;
-	bunny.y = 10;
-
-	// Add the bunny to the scene we are building
-	app.stage.addChild(bunny);
-
+	for (let layerData of layers) {
+		const index = layerData["layer"]
+		const tiles = layerData["tiles"]
+		drawGrid(index, tiles)
+	}
 });
